5fdee7975af984f35fbb44b23bb1d905f53b6225
chandiosamad07 <absamadchandio@gmamil.com>
2024-05-24 11:31:58 +0500

    Java Variables

--- javaBasic/src/javaBasic/Test.java
+++ javaBasic/src/javaBasic/Test.java
@@ -18,8 +18,11 @@ public class Test {
 		to the screen, and it is amazing */
 		System.out.println("Hello World");
 		
-		String name = "John";
-		System.out.println(name);
+
+		String firstName = "John ";
+		String lastName = "Doe";
+		String fullName = firstName + lastName;
+		System.out.println(fullName);
 
 //		int myNum;
 //		myNum = 15;
@@ -32,6 +35,26 @@ public class Test {
 		String myText = "Hello";
 		
 		System.out.println(myNum+" "+myFloatNum+" "+myLetter +" "+myBool +" "+myText);
+		
+		int x = 5;
+		int y = 6;
+		int z = 50;
+		System.out.println(x + y + z);
+		
+		// Student data
+		String studentName = "John Doe";
+		int studentID = 15;
+		int studentAge = 23;
+		float studentFee = 75.25f;
+		char studentGrade = 'B';
+
+		// Print variables
+		System.out.println("Student name: " + studentName);
+		System.out.println("Student id: " + studentID);
+		System.out.println("Student age: " + studentAge);
+		System.out.println("Student fee: " + studentFee);
+		System.out.println("Student grade: " + studentGrade);
+
 	}
 	
 
