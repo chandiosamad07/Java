9b79cbaf084787e86fa049b45b4aa28a0a6a14f0
chandiosamad07 <absamadchandio@gmamil.com>
2024-05-24 15:42:47 +0500

    Type casting in java

--- javaBasic/src/javaBasic/TypeCasting.java
+++ javaBasic/src/javaBasic/TypeCasting.java
@@ -4,10 +4,20 @@ public class TypeCasting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Widening Casting
 		int myInt =9;
 		double myDouble = myInt;
 		System.out.println(myInt);
 		System.out.println(myDouble);
+		
+		//Narrowing Casting
+		double myDb = 9.78;
+		int myIT = (int) myDouble;
+		System.out.println(myIT);
+		System.out.println(myDb);
+		
+		
+		
 
 	}
 
