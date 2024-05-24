e90b78e436de7d44592cb107c17502de22813b9a
chandiosamad07 <absamadchandio@gmamil.com>
2024-05-24 15:50:08 +0500

    operators in java

---
+++ javaBasic/src/javaBasic/JavaOperator.java
@@ -0,0 +1,21 @@
+package javaBasic;
+
+public class JavaOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=10;
+		System.out.println("The sum of "+a+" and "+b+ " = "+(a+b));
+		System.out.println("The Minsu of "+a+" and "+b+ " = "+(a-b));
+		System.out.println("The Multiple of "+a+" and "+b+ " = "+(a*b));
+		System.out.println("The Division of "+a+" and "+b+ " = "+(a/b));
+		System.out.println("The Mod of "+a+" and "+b+ " = "+(a%b));
+		
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+	}
+
+}
