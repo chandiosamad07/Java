1f58413132727bb850131509ca0eb0bc48234411
chandiosamad07 <absamadchandio@gmamil.com>
2024-05-29 09:36:52 +0500

    Java Math

---
+++ javaBasic/src/javaBasic/Maths.java
@@ -0,0 +1,15 @@
+package javaBasic;
+
+public class Maths {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.max(5, 10)); 
+		System.out.println(Math.min(5, 10)); 
+		System.out.println(Math.sqrt(64));
+		System.out.println(Math.abs(-4.7));
+		int randomNum = (int)(Math.random() * 101);  // 0 to 100
+	    System.out.println(randomNum);
+	}
+
+}
